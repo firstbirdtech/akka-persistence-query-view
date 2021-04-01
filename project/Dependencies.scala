550ba48a0661550ff5534f843ea5836fa45da7da
Scala Steward <me@scala-steward.org>
2021-04-01 08:27:37 +0200

    Update scalatest to 3.2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     "org.slf4j"          % "slf4j-api"              % "1.7.30",
     "com.typesafe"       % "config"                 % "1.4.1",
     // -- Testing --
-    "org.scalatest"     %% "scalatest"           % "3.2.4"     % Test,
+    "org.scalatest"     %% "scalatest"           % "3.2.7"     % Test,
     "org.scalatestplus" %% "scalacheck-1-14"     % "3.2.2.0"   % Test,
     "com.typesafe.akka" %% "akka-slf4j"          % akkaVersion % Test,
     "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % Test,
