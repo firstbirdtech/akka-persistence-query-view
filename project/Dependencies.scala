32c1544f70430dd056503ecc58001592b62913cc
Scala Steward <me@scala-steward.org>
2022-02-02 14:28:45 +0100

    Update typesafe:config to 1.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-stream"            % akkaVersion,
     "ch.qos.logback"     % "logback-classic"        % "1.1.8",
     "org.slf4j"          % "slf4j-api"              % "1.7.35",
-    "com.typesafe"       % "config"                 % "1.4.1",
+    "com.typesafe"       % "config"                 % "1.4.2",
     // -- Testing --
     "org.scalatest"     %% "scalatest"           % "3.2.4"     % Test,
     "org.scalatestplus" %% "scalacheck-1-14"     % "3.2.2.0"   % Test,
