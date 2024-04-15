8d00a90fc4ff0d8cf1e443ec9eab593ab2a089c6
Scala Steward <scala_steward@virtuslab.com>
2024-04-15 15:32:46 +0000

    Update slf4j-api to 2.0.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-persistence-query" % akkaVersion,
     "com.typesafe.akka" %% "akka-stream"            % akkaVersion,
     "ch.qos.logback"     % "logback-classic"        % "1.1.8",
-    "org.slf4j"          % "slf4j-api"              % "2.0.12",
+    "org.slf4j"          % "slf4j-api"              % "2.0.13",
     "com.typesafe"       % "config"                 % "1.4.3",
     // -- Testing --
     "org.scalatest"     %% "scalatest"           % "3.2.4"     % Test,
